148d738aeaaf5931fc13ad3a0fed892bbc2e7189
ankit <59723644+ank-web@users.noreply.github.com>
2022-08-31 18:42:29 +0530

    Updates

---
+++ precache-manifest.046aa9734a7d892aa783589774ed7598.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32e73dadb95b02dd49b9631496517e3",
+    "url": "/IPod-Music/index.html"
+  },
+  {
+    "revision": "05fdf55ec07a83d1a2ab",
+    "url": "/IPod-Music/static/css/main.167ba9fe.chunk.css"
+  },
+  {
+    "revision": "b33247460bcc298fc2b6",
+    "url": "/IPod-Music/static/js/2.9cc1cb72.chunk.js"
+  },
+  {
+    "revision": "d8341c57f580e145d476bfdbdb6453b4",
+    "url": "/IPod-Music/static/js/2.9cc1cb72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fdf55ec07a83d1a2ab",
+    "url": "/IPod-Music/static/js/main.c2ec86d4.chunk.js"
+  },
+  {
+    "revision": "9e6f18ccfeb6fae06457",
+    "url": "/IPod-Music/static/js/runtime-main.a911ee28.js"
+  },
+  {
+    "revision": "785ef849cec59d32b3783ea2db575b76",
+    "url": "/IPod-Music/static/media/Chaand Baaliyan.785ef849.mp4"
+  },
+  {
+    "revision": "7b43e2624c8ad56458e5b915926993a5",
+    "url": "/IPod-Music/static/media/Excuses.7b43e262.mp4"
+  },
+  {
+    "revision": "2e4d210d6f8fa9b1bf2390493f31cc89",
+    "url": "/IPod-Music/static/media/KAUN TUJHE.2e4d210d.mp4"
+  },
+  {
+    "revision": "f21646ebc17ddb84ef81110fa81180a1",
+    "url": "/IPod-Music/static/media/Kesariya.f21646eb.mp4"
+  },
+  {
+    "revision": "f8596fae636b7fefec27ef478e4e9d51",
+    "url": "/IPod-Music/static/media/Raataan Lambiyan.f8596fae.mp4"
+  },
+  {
+    "revision": "9274c94d78bd73c6b7b1ec7c09f6113f",
+    "url": "/IPod-Music/static/media/chaand baliyan.9274c94d.jpg"
+  },
+  {
+    "revision": "3b5941f94bf06840bef6658df058918c",
+    "url": "/IPod-Music/static/media/excuses.3b5941f9.jpg"
+  },
+  {
+    "revision": "0f702e6d9615af4ea6e4503fa3754645",
+    "url": "/IPod-Music/static/media/game.0f702e6d.jpg"
+  },
+  {
+    "revision": "423c0b4f1c0c517fccad0c3e50de3425",
+    "url": "/IPod-Music/static/media/kaun tujhe.423c0b4f.jpg"
+  },
+  {
+    "revision": "4fc20c44b394a2bacbffc5bbd5c42102",
+    "url": "/IPod-Music/static/media/kesariya.4fc20c44.jpg"
+  },
+  {
+    "revision": "86f652825909378dd28f81eb87ed4dd3",
+    "url": "/IPod-Music/static/media/music.86f65282.jpg"
+  },
+  {
+    "revision": "14a8cbbcfdb2a65e3e0f5ea0f73c4f34",
+    "url": "/IPod-Music/static/media/raatan-lambiyan.14a8cbbc.jpg"
+  },
+  {
+    "revision": "626bd78a090cd747f7bfe1addd07babd",
+    "url": "/IPod-Music/static/media/settings.626bd78a.png"
+  },
+  {
+    "revision": "227f0ba9833ea10638eb851b4937d9f3",
+    "url": "/IPod-Music/static/media/wallpaper1.227f0ba9.jpg"
+  },
+  {
+    "revision": "23077a93618a97f5d7254432926d3d69",
+    "url": "/IPod-Music/static/media/wallpaper2.23077a93.jpg"
+  },
+  {
+    "revision": "b2fda5cab36fb65eccf326028ec5e1bb",
+    "url": "/IPod-Music/static/media/wallpaper3.b2fda5ca.jpg"
+  }
+]);
